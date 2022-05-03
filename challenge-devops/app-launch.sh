0c6aa82383b80bc483bf98105a666450f40e02b8
Ricorhy <mico.angelo.b.rico@accenture.com>
2022-05-03 12:55:40 +0800

    DevOps Patch Update

--- challenge-devops/app-launch.sh
+++ challenge-devops/app-launch.sh
@@ -1,8 +1,10 @@
 #!/bin/bash
 
-kubectl apply -f postgres-pv.yaml #launch postgres persistent volume to cluster and apply persistent volume claim
+kubectl apply -f postgres-pv.yaml #create postgres persistent volume and apply persistent volume claim
 
-kubectl apply -f postgres-config.yaml #launch postgres config changes to cluster
+kubectl apply -f postgres-config.yaml #launch postgres config changes 
+
+kubectl apply -f start-app.yaml #deploy k8 containers
 
 kubectl apply -f postgres-service.yaml #launch postgres service to cluster
 
